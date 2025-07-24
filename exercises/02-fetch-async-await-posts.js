ac9347a3b6942c293b1829ff5d2065e6762bda9b
nongnapasbb <nongnapasbb@gmail.com>
2025-07-24 16:29:49 +0700

    finished exercise2

--- exercises/02-fetch-async-await-posts.js
+++ exercises/02-fetch-async-await-posts.js
@@ -8,3 +8,30 @@ TODO-01: Create a function named fetchPostsAsync
 
 TODO-02: Export fetchPostsAsync and test it in main.js 
 */
+const onSuccess = (data) => {
+	console.log("✅ Data fetched successfully:", data);
+};
+
+const onFailure = (error) => {
+	console.error("❌ Error fetching data:", error.message);
+};
+
+export const fetchPostsAsync = async () => {
+  const endpoint = "https://dummyjson.com/posts";
+  try {
+    const response = await fetch(endpoint);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    onSuccess(data);
+  } catch (error) {
+    onFailure(error);
+  } finally {
+    console.log("ℹ️ Fetch operation completed.");
+  }
+};
+
+fetchPostsAsync();
